4f1ff3340559f0519f651d73cc0a2e89d649bac0
bbtit <the8butterfly8dream@gmail.com>
2023-08-28 12:25:52 +0900

    Rewriting to OOP

--- modules/req.py
+++ modules/req.py
@@ -35,5 +35,52 @@ async def main(file_path, url):
             )
 
 
+class FileUploader:
+    def __init__(self, file_path_queue, url):
+        self.file_path_queue = file_path_queue
+        self.url = url
+
+    async def process_queue(self):
+        async with aiohttp.ClientSession() as session:
+            while True:
+                file_path = await self.file_path_queue.get()
+                asyncio.create_task(self.post_file(session, file_path))
+                self.file_path_queue.task_done()
+
+    async def post_file(self, session, file_path):
+        async with aiofiles.open(file_path, "rb") as file:
+            data = await file.read()
+            files = {"file": ("weather.mp3", data)}
+            async with session.post(self.url, data=files) as response:
+                json_response = await response.json()
+                print(f"{json_response}")
+
+    async def start(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                self.url, data={"file": open(file_path, "rb")}
+            ) as response:
+                print(response)
+                json_response = await response.json()
+                print(
+                    f"""File Path: {file_path},
+                    \nStatus Code: {response.status},
+                    \nJSON Content: {json_response}"""
+                )
+
+
+async def main2(file_path, url):
+    file_path_queue = asyncio.Queue()
+    file_path_queue.put_nowait(file_path)
+
+    uploader = FileUploader(file_path_queue, url)
+    await asyncio.gather(
+        uploader.process_queue(),
+        uploader.start(),
+    )
+
+
 if __name__ == "__main__":
-    asyncio.run(main("weather.mp3", "http://192.168.11.53:80/api/transcribe"))
+    file_path = "weather.mp3"
+    url = "http://192.168.11.53:80/api/transcribe"
+    asyncio.run(main(file_path, url))
