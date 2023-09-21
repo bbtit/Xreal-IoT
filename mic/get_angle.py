ab1e3dc22583f47a99d8a1fba14fb2866e686996
bbtit <the8butterfly8dream@gmail.com>
2023-09-21 13:11:57 +0900

    Add mic/get_angle.py

---
+++ mic/get_angle.py
@@ -0,0 +1,11 @@
+from modules.record import SoundRecorder
+from time import sleep
+from queue import Queue
+
+if __name__ == "__main__":
+    file_path_queue = Queue()
+    voice_angle_queue = Queue()
+    recorder = SoundRecorder(file_path_queue, voice_angle_queue)
+    while True:
+        recorder.read_parameter("DOAANGLE")
+        sleep(1)
