2f4b7da38cf7aca984f7db69ef985a17d222014f
sntk000 <takumi.senaha000@gmail.com>
2025-05-01 16:43:19 +0900

    Revert "refactor: simplify voice angle retrieval and adjust text rendering font size"
    This reverts commit 481fba3d5ac87cb3d94828b1c5f4dc3ad9cb392c.

--- modules/window.py
+++ modules/window.py
@@ -66,7 +66,9 @@ class WindowCanvasManager:
         # TODO Queueから何回かNoneを取得したら円弧とテキストを削除する
         # Queueから角度を取得(ノンブロッキング)
         input_degree: int = (
-            voice_angle_queue.get_nowait() if not voice_angle_queue.empty() else None
+            voice_angle_queue.get_nowait()
+            if not voice_angle_queue.empty()
+            else None
         )
 
         # Queueからテキストを取得(ノンブロッキング)
@@ -107,16 +109,13 @@ class WindowCanvasManager:
 
         if input_text is not None:
             # テキストが描画されていない場合は描画する
-            font_size = 54
-            y_offset = 100 + (font_size - 24)  # フォントサイズ増加分を考慮
-
             if self.drown_text_id is None:
                 self.create_text(
                     self.window_center[0],
-                    self.window_center[1] + self.circle_radius + y_offset,
+                    self.window_center[1] + self.circle_radius + 100,
                     text="Hello, world!",
                     fill="red",
-                    font=("Arial", font_size),
+                    font=("Arial", 24),
                     anchor="center",
                 )
             # テキストを更新
